2ec83535b9bc93f6abfcd783a60197d640b3988c
Benncio09 <lorebencini13@outlook.com>
2024-10-31 10:45:23 +0100

    form quasi completo, manca da aggiungere div per biglietti in piu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function biglietto_unico() {
+    document.getElementById("quantita_biglietti_div").style.display = "none";
+
+
+}
+
+function piu_biglietti() {
+    document.getElementById("quantita_biglietti_div").style.display = "block";
+
+}
